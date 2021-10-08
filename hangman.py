41b869c530296905ee285db048b4dbb1f1d90090
Roibéard R Coughlan <roibeard.c@outlook.com>
2021-10-08 07:15:10 +0000

    Complete first image of the 6/7 hangman stage's to be represented in the command after each guess

--- hangman.py
+++ hangman.py
@@ -2,3 +2,34 @@ import random
 from words import list_of_words
 
 print(list_of_words)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def hangman_display(tries):
+    stages = [  # final state: head, torso, both arms, and both legs
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                """,
